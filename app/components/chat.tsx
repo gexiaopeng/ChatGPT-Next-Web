2cf5298367a062269c3df5dc7269b4a7eed58850
gexiaopeng <gexiaopeng@tom.com>
2023-04-19 16:34:35 +0800

    ok

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -659,7 +659,7 @@ export function Chat(props: {
               icon={<ReturnIcon />}
               bordered
               title={Locale.Chat.Actions.ChatList}
-              onClick={props?.showSideBar}
+              onClick={props?.setShowSettings}
             />
           </div>
           <div className={styles["window-action-button"]}>
