6f64871fe66a8c25b9ae9a88c423eaef8eee1a5f
gexiaopeng <gexiaopeng@tom.com>
2023-04-20 10:12:40 +0800

    ok

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -367,6 +367,7 @@ export function ChatActions(props: {
       >
        <SettingsIcon
            onClick={() => {
+             console.log("---myporps---",myporps);
              props?.showSettings();
            }}
         />
