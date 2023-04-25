ba67369347300a8ad3c65b350a6cbb407af7c823
gexiaopeng <gexiaopeng@tom.com>
2023-04-25 13:57:22 +0800

    ok

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -12,7 +12,7 @@ import styles from "./home.module.scss";
 import SettingsIcon from "../icons/settings.svg";
 import GithubIcon from "../icons/github.svg";
 import ChatGptIcon from "../icons/chatgpt.svg";
-import ClosegIcon from "../icons/menug.svg";
+import ClosegIcon from "../icons/closeg.svg";
 
 import BotIcon from "../icons/bot.svg";
 import AddIcon from "../icons/add.svg";
