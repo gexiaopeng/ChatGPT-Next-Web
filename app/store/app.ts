f68bf24e490b32d4ae1b33f957fa5979dc6bd884
gexiaopeng <gexiaopeng@tom.com>
2023-04-25 16:40:57 +0800

    ok

--- app/store/app.ts
+++ app/store/app.ts
@@ -456,11 +456,9 @@ export const useChatStore = create<ChatStore>()(
             // stream response
             if (done) {
               botMessage.streaming = false;
-              content = content.trim().replace(/^\r?\n/, "");
+
               if(role==101) {
-                content = content.trim().replace(/^Translation:/, "");
-                content = content.trim().replace(/^\"/, "");
-                content = content.trim().replace(/\"$/, "");
+               content = content.trim().replace(/\"$/, "");
               }
               botMessage.content = content;
               get().onNewMessage(botMessage);
@@ -469,7 +467,12 @@ export const useChatStore = create<ChatStore>()(
                 botMessage.id ?? messageIndex,
               );
             } else {
-              console.log("content:["+content+"]");
+              //console.log("content:["+content+"]");
+              content = content.trim().replace(/^\r?\n/, "");
+              if(role==101) {
+                content = content.trim().replace(/^Translation:/, "");
+                content = content.trim().replace(/^\"/, "");
+              }
               botMessage.content = content;
               set(() => ({}));
             }
