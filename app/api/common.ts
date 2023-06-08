0ac79620ae99ee1c6c4f90ed2c673b403487cd12
gexiaopeng <gexiaopeng@tom.com>
2023-06-08 11:00:59 +0800

    ok

--- app/api/common.ts
+++ app/api/common.ts
@@ -2,18 +2,18 @@ import { NextRequest } from "next/server";
 
 const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
-const DEFAULT_PATH="v1/chat/completions";
+const DEFAULT_PATH = "v1/chat/completions";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
-let seq=0;
+let seq = 0;
 export async function requestOpenai(req: NextRequest) {
   const token = req.headers.get("token");
   const tokens = token?.split(",") ?? [];
-  let len=tokens.length;
-  if(seq>=len){
-    seq=0;
+  let len = tokens.length;
+  if (seq >= len) {
+    seq = 0;
   }
-  const apiKey=tokens[seq].trim();
+  const apiKey = tokens[seq].trim();
   seq++;
   const openaiPath = req.headers.get("path") || DEFAULT_PATH;
   let baseUrl = BASE_URL;
@@ -23,23 +23,33 @@ export async function requestOpenai(req: NextRequest) {
 
   console.log("[Proxy] ", openaiPath);
   console.log("[Base Url]", baseUrl);
-  let res=  await  fetch(`${baseUrl}/${openaiPath}`, {
+  let res = await fetch(`${baseUrl}/${openaiPath}`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${apiKey}`,
     },
     method: req.method,
     body: req.body,
+    duplex: "half",
   });
-  let key=apiKey.slice(-4);
+  let key = apiKey.slice(-4);
   //res={...res,...{key}};
- // await res.headers.set('token', key);
- // console.log("[requestOpenai headers] ",  await res.headers);
+  // await res.headers.set('token', key);
+  // console.log("[requestOpenai headers] ",  await res.headers);
   const newHeaders = await new Headers(await res.headers);
-  newHeaders.set('token', key);
- // console.log("[requestOpenai newHeaders] ",  await newHeaders);
-  let body=await res.body;
-  console.log("[requestOpenai status] ",  res.status,res.statusText,openaiPath);
-  const modifiedRes = await new Response(await body, { status: res.status, statusText: res.statusText, headers: newHeaders });
+  newHeaders.set("token", key);
+  // console.log("[requestOpenai newHeaders] ",  await newHeaders);
+  let body = await res.body;
+  console.log(
+    "[requestOpenai status] ",
+    res.status,
+    res.statusText,
+    openaiPath,
+  );
+  const modifiedRes = await new Response(await body, {
+    status: res.status,
+    statusText: res.statusText,
+    headers: newHeaders,
+  });
   return modifiedRes;
 }
